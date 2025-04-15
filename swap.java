f032861697eaebae63e0bde2ab63b5790df907ac
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-15 10:10:56 +0530

    file created

---
+++ swap.java
@@ -0,0 +1,18 @@
+package swap;
+
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner();
+        System.out.println("enter first value");
+        int num1;
+        System.out.println("enter second value");
+        int num2;
+        System.out.println("before swapping");
+        num1=next.Scanner();
+        System.out.println("after swapping");
+        num2=next.Scanner();
+    }
+    
+}
